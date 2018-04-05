40817d50aa18fe82b9108f3eb38c0847b2e489e1
Sunha Ryu <shryu@dinnerqueen.net>
2018-04-05 15:57:00 +0900

    크롤링 결과를 database에 저장한다

--- myapp/management/commands/getflowers.py
+++ myapp/management/commands/getflowers.py
@@ -1,7 +1,8 @@
 from django.core.management import BaseCommand
+from myapp.models import Flower
 
 class Command(BaseCommand):
-    help = 'get flowers from wiki'
+    help = 'get flowers from wikipedia'
 
     def handle(self, *args, **options):
         import requests
@@ -29,13 +30,34 @@ class Command(BaseCommand):
             elements = soup.select(selector)
             return elements
 
+        print('--- 시작 ---')
         # 위키백과 원예 식물 분류 url
         url = "https://ko.wikipedia.org/wiki/%EB%B6%84%EB%A5%98:%EC%9B%90%EC%98%88_%EC%8B%9D%EB%AC%BC"
         # html text를 가져온다
         html = get_text(url)
         # BeautifulSoup Element List를 가져온다.
         elements = get_elements(html, ".mw-category-group > ul > li > a")
+        add_flower_list = []
 
-        for element in elements:
+        # 이미 존재하는 꽃들을 제외시키기 위해 테이블의 내용을 가져온다.
+        all_flower_list = Flower.objects.all()
+        flower_list = []
+        for flower_obj in all_flower_list:
+            flower_list.append(flower_obj.name)
+
+        for i, element in enumerate(elements):
             # 꽃 이름을 화면에 출력한다.
-            print(element.get_text())
+            flower_name = element.get_text()
+
+            # 이미 등록된 꽃이면 continue
+            if flower_name in flower_list:
+                print(str(i) + ') ' + flower_name + ' is Already Exist')
+                continue
+
+            flower_obj = Flower(name=flower_name)
+            add_flower_list.append(flower_obj)
+
+            print(str(i) + ') ' + flower_name)
+
+        res = Flower.objects.bulk_create(add_flower_list)
+        print('--- 종료 ---')
